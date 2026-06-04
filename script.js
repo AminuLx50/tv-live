5bc2401f1243d540918bcdd76bb49d8ad699f2d7
AminuLx50 <amrofficial360@gmail.com>
2026-06-05 01:34:31 +0600

    Rename to AminuL TV

--- script.js
+++ script.js
@@ -1,1 +1,66 @@
-console.log("TV Live");
+const video = document.getElementById("video");
+const channelList = document.getElementById("channelList");
+
+let channels = [];
+
+fetch("channels.json")
+.then(res => res.json())
+.then(data => {
+
+    channels = data;
+
+    renderChannels(data);
+
+});
+
+function renderChannels(data){
+
+    channelList.innerHTML = "";
+
+    data.forEach(channel => {
+
+        const div = document.createElement("div");
+
+        div.className = "channel";
+
+        div.innerHTML = channel.name;
+
+        div.onclick = () => playChannel(channel.url);
+
+        channelList.appendChild(div);
+
+    });
+
+}
+
+function playChannel(url){
+
+    if(Hls.isSupported()){
+
+        const hls = new Hls();
+
+        hls.loadSource(url);
+
+        hls.attachMedia(video);
+
+    }else{
+
+        video.src = url;
+
+    }
+
+}
+
+document
+.getElementById("search")
+.addEventListener("input", e => {
+
+    const value = e.target.value.toLowerCase();
+
+    const filtered = channels.filter(ch =>
+        ch.name.toLowerCase().includes(value)
+    );
+
+    renderChannels(filtered);
+
+});
