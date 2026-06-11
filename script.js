ac942dbcebcb36415989858d76d4a6e767a5eb7c
AminuLx50 <amrofficial360@gmail.com>
2026-06-11 19:48:34 +0600

    Fixed channel loading

--- script.js
+++ script.js
@@ -1,9 +1,40 @@
+const video = document.getElementById("video");
+const channelList = document.getElementById("channelList");
+const search = document.getElementById("search");
+
+let channels = [];
+let hls = null;
+
+function playChannel(url) {
+    if (hls) {
+        hls.destroy();
+    }
+
+    if (Hls.isSupported()) {
+        hls = new Hls();
+        hls.loadSource(url);
+        hls.attachMedia(video);
+
+        hls.on(Hls.Events.MANIFEST_PARSED, () => {
+            video.play().catch(() => {});
+        });
+
+    } else if (video.canPlayType("application/vnd.apple.mpegurl")) {
+        video.src = url;
+        video.play().catch(() => {});
+    }
+}
+
 function renderChannels(data) {
     channelList.innerHTML = "";
 
+    if (data.length === 0) {
+        channelList.innerHTML = "<p style='padding:15px'>No channels found</p>";
+        return;
+    }
+
     data.forEach(channel => {
         const div = document.createElement("div");
-
         div.className = "channel";
 
         div.innerHTML = `
@@ -18,3 +49,39 @@ function renderChannels(data) {
         channelList.appendChild(div);
     });
 }
+
+fetch("./channels.json")
+    .then(res => {
+        if (!res.ok) {
+            throw new Error("channels.json not found");
+        }
+        return res.json();
+    })
+    .then(data => {
+        channels = data;
+
+        renderChannels(channels);
+
+        if (channels.length > 0) {
+            playChannel(channels[0].url);
+        }
+    })
+    .catch(err => {
+        console.error(err);
+
+        channelList.innerHTML = `
+            <div style="padding:15px;color:red">
+                Failed to load channels.json
+            </div>
+        `;
+    });
+
+search.addEventListener("input", () => {
+    const value = search.value.toLowerCase();
+
+    const filtered = channels.filter(channel =>
+        channel.name.toLowerCase().includes(value)
+    );
+
+    renderChannels(filtered);
+});
