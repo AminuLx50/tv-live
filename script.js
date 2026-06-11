b27811aa8d6ec99ea1b92664343f5eb7bd496fb8
AminuLx50 <amrofficial360@gmail.com>
2026-06-11 21:19:56 +0600

    Restore original site

--- script.js
+++ script.js
@@ -6,102 +6,67 @@ let channels = [];
 let hls = null;
 
 function playChannel(url) {
-if (hls) {
-hls.destroy();
-}
+    if (hls) {
+        hls.destroy();
+    }
 
-```
-if (Hls.isSupported()) {
-    hls = new Hls({
-        maxBufferLength: 5,
-        maxMaxBufferLength: 10
-    });
+    if (Hls.isSupported()) {
+        hls = new Hls();
+        hls.loadSource(url);
+        hls.attachMedia(video);
 
-    hls.loadSource(url);
-    hls.attachMedia(video);
+        hls.on(Hls.Events.MANIFEST_PARSED, () => {
+            video.play().catch(() => {});
+        });
 
-    hls.on(Hls.Events.MANIFEST_PARSED, () => {
+    } else if (video.canPlayType("application/vnd.apple.mpegurl")) {
+        video.src = url;
         video.play().catch(() => {});
-    });
-
-} else if (video.canPlayType("application/vnd.apple.mpegurl")) {
-    video.src = url;
-    video.play().catch(() => {});
-}
-```
-
+    }
 }
 
 function renderChannels(data) {
-channelList.innerHTML = "";
+    channelList.innerHTML = "";
 
-```
-if (data.length === 0) {
-    channelList.innerHTML =
-        "<p style='padding:15px'>No channels found</p>";
-    return;
-}
+    if (data.length === 0) {
+        channelList.innerHTML = "<p style='padding:15px'>No channels found</p>";
+        return;
+    }
 
-data.forEach(channel => {
-    const div = document.createElement("div");
+    data.forEach(channel => {
+        const div = document.createElement("div");
+        div.className = "channel";
 
-    div.className = "channel";
+        div.innerHTML = `
+            <img src="${channel.logo}" class="channel-logo" alt="${channel.name}">
+            <span>${channel.name}</span>
+        `;
 
-    div.innerHTML = `
-        <img src="${channel.logo}" class="channel-logo" alt="${channel.name}">
-        <span>${channel.name}</span>
-    `;
+        div.addEventListener("click", () => {
+            playChannel(channel.url);
+        });
 
-    div.addEventListener("click", () => {
-        playChannel(channel.url);
+        channelList.appendChild(div);
     });
-
-    channelList.appendChild(div);
-});
-```
-
 }
 
 fetch("./channels.json")
-.then(res => res.json())
-.then(data => {
-channels = data;
-renderChannels(channels);
+    .then(res => res.json())
+    .then(data => {
+        channels = data;
+        renderChannels(channels);
 
-```
-    if (channels.length > 0) {
-        playChannel(channels[0].url);
-    }
-})
-.catch(err => {
-    console.error(err);
-});
-```
+        if (channels.length > 0) {
+            playChannel(channels[0].url);
+        }
+    });
 
 search.addEventListener("input", () => {
-const value = search.value.toLowerCase();
+    const value = search.value.toLowerCase();
 
-```
-const filtered = channels.filter(channel =>
-    channel.name.toLowerCase().includes(value)
-);
+    const filtered = channels.filter(channel =>
+        channel.name.toLowerCase().includes(value)
+    );
 
-renderChannels(filtered);
-```
-
-});
-
-/* Hide Loader */
-
-window.addEventListener("load", () => {
-setTimeout(() => {
-const loader = document.getElementById("loader");
-
-```
-    if (loader) {
-        loader.style.display = "none";
-    }
-}, 2000);
-```
-
+    renderChannels(filtered);
 });
